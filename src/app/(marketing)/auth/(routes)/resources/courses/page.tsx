6d066adab7f0eeeea40026f84850781ad9dabf7e
Aditya Kumar <132904608+ErAditya1@users.noreply.github.com>
2025-04-18 19:35:50 +0530

    build error fixed

--- src/app/(marketing)/auth/(routes)/resources/courses/page.tsx
+++ src/app/(marketing)/auth/(routes)/resources/courses/page.tsx
@@ -3,12 +3,12 @@ import Courses from '@/app/(marketing)/components/courses/Course';
 import AnimationContainer from '@/app/(marketing)/components/global/animation-container';
 import React from 'react'
 
-const BlogPage = () => {
+const CoursePage = () => {
     return (
         <div className="flex flex-col items-center justify-center pb-20">
             <AnimationContainer delay={0.1} className="w-full">
                 <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold font-heading text-center mt-6 !leading-tight">
-                    Blog
+                    Courses
                 </h1>
                 <p className="text-base md:text-lg mt-6 text-center text-muted-foreground">
                     Latest Courses .
@@ -21,4 +21,4 @@ const BlogPage = () => {
     )
 };
 
-export default BlogPage
+export default CoursePage
--- src/app/(marketing)/components/courses/Course.tsx
+++ src/app/(marketing)/components/courses/Course.tsx
@@ -1,17 +1,24 @@
 
 import api from "@/api";
 import MarketingCourseCard from "../MarketingCourseCard";
-
+interface Course {
+    _id: string;
+    // add other props used in MarketingCourseCard
+  }
 const Courses = async() => {
 
-    const res = await api.patch("/v1/courses/course/getAllCourses");
-    const courses = res.data.data || [];
-    console.log(courses)
+    let courses: Course[] = [];
+try {
+  const res = await api.get("/v1/courses/course/getAllCourses");
+  courses = res.data.data || [];
+} catch (error) {
+  console.error("Failed to fetch courses:", error);
+}
     return (
         <div className="flex flex-col items-center justify-center max-w-6xl mx-auto px-4 md:px-0">
             <div className="grid xs:grid-cols-2 md:grid-cols-3 mt-4 lg:grid-cols-3 xl:grid-cols-4 gap-4">
                  {
-                  courses?.map((course:any , index:number) => (
+                  courses?.map((course:Course , index:number) => (
                     
                     <MarketingCourseCard  key = {course?._id} _id = {course?._id}/>
                     
