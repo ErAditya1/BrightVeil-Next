5aeb79337308d7abd986d0c231c5f7f40b6838f9
Aditya Kumar <132904608+ErAditya1@users.noreply.github.com>
2025-04-18 19:51:53 +0530

    error fixed

--- src/app/(marketing)/components/courses/Course.tsx
+++ src/app/(marketing)/components/courses/Course.tsx
@@ -1,32 +1,36 @@
-
+'use client'
 import api from "@/api";
 import MarketingCourseCard from "../MarketingCourseCard";
+import { useEffect, useState } from "react";
 interface Course {
-    _id: string;
-    // add other props used in MarketingCourseCard
-  }
-const Courses = async() => {
-
-    let courses: Course[] = [];
-try {
-  const res = await api.get("/v1/courses/course/getAllCourses");
-  courses = res.data.data || [];
-} catch (error) {
-  console.error("Failed to fetch courses:", error);
+  _id: string;
+  // add other props used in MarketingCourseCard
 }
-    return (
-        <div className="flex flex-col items-center justify-center max-w-6xl mx-auto px-4 md:px-0">
-            <div className="grid xs:grid-cols-2 md:grid-cols-3 mt-4 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                 {
-                  courses?.map((course:Course , index:number) => (
-                    
-                    <MarketingCourseCard  key = {course?._id} _id = {course?._id}/>
-                    
-                   ))
-                 }
-                  </div>
-        </div>
-    )
+const Courses = async () => {
+    const [courseData, setCourseData] = useState([]) 
+   
+    useEffect( () => {
+          api.patch("/v1/courses/course/getAllCourses")
+        .then((res) => {
+          console.log(res)
+          setCourseData(res.data.data)
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+      
+        
+      
+    },[])
+  return (
+    <div className="flex flex-col items-center justify-center max-w-6xl mx-auto px-4 md:px-0">
+      <div className="grid xs:grid-cols-2 md:grid-cols-3 mt-4 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        {courseData?.map((course: Course, index: number) => (
+          <MarketingCourseCard key={course?._id} _id={course?._id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Courses
+export default Courses;
